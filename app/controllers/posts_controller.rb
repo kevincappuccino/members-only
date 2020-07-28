bd3e219363b17ef77ba4175bc601c7504c5be456
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2020-07-28 09:58:41 +0000

    Finish user registration controllerplus session

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[show edit update destroy]
   before_action :authenticate_user!, except: %i[index show]
   def index
     @posts = Post.all
